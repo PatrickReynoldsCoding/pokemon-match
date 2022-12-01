aaa54d199fc06f4273b1d1ed18ec718c364eabd3
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-12-01 14:40:26 +0700

    pokemon selector complete

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React, { useState } from "react";
 
 // // random pokemon number generator selector
 
-const cardImages = [];
+let cardImages = [];
+let usedPokemon = [];
 
 for (let i = 0; i < 6; i++) {
+  //random number selector between 1-151
   const randPokeSelector = () => {
-    let randNum = Math.floor(Math.random() * (145 - 1 + 1) + i);
+    let randNum = Math.floor(Math.random() * 145 + i);
     if (randNum.toString().length === 3) return randNum.toString();
     if (randNum.toString().length === 2) return `0${randNum}`;
     if (randNum.toString().length === 1) return `00${randNum}`;
   };
 
+  let currentPokemon = randPokeSelector();
+
+  if (!usedPokemon.includes(currentPokemon)) usedPokemon.push(currentPokemon);
   cardImages.push({
-    src: `/card_images/${randPokeSelector()}.png`,
+    src: `/card_images/${currentPokemon}.png`,
     matched: true,
   });
 }
