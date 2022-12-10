1eef2aec4147c2f168252e4f6bd8ca779b251498
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-12-10 16:22:10 +0700

    animation now working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
-// Components
+//components
 import Card from "./Card";
+
+const cardImages = [
+  { src: "/img/helmet-1.png", matched: true },
+  { src: "/img/potion-1.png", matched: true },
+  { src: "/img/ring-1.png", matched: true },
+  { src: "/img/scroll-1.png", matched: true },
+  { src: "/img/shield-1.png", matched: true },
+  { src: "/img/sword-1.png", matched: true },
+];
 
 function App() {
   const [cards, setCards] = useState([]);
@@ -11,6 +20,7 @@ function App() {
   const [choice2, setChoice2] = useState(null);
   const [cardEnabler, setCardEnabler] = useState(true);
 
+  // shuffle cards for new game
   // random pokemon number generator selector
   const shuffleCards = () => {
     let sixChosenMons = [];
@@ -53,34 +63,50 @@ function App() {
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }));
     // console.log(fullDeck);
+    setChoice1(null);
+    setChoice2(null);
     setCards(fullDeck);
-  };
-
-  //Start Game
-  useEffect(() => {
-    shuffleCards();
+    setTurns(0);
     setTimeout(() => {
       gameStartCardFlipper();
-    }, 2000);
-  }, []);
-
-  // load card to choice states
-  const handleChoice = (card) => {
-    choice1 ? setChoice2(card) : setChoice1(card);
-    console.log(choice1);
-    console.log(choice2);
+    }, 5000);
   };
 
-  // run match function if 2 cards are picked
-  useEffect(() => {
-    bombCheck(choice1, choice2);
-    reviewChoices(choice1, choice2);
+  // remember choice
+  const handleChoice = (card) => {
+    choice1 ? setChoice2(card) : setChoice1(card);
+  };
 
-    //end game function if bombCheck is picked
+  //review choices
+  useEffect(() => {
+    reviewChoices(choice1, choice2);
   }, [choice1, choice2]);
 
-  // checks if the two cards match
   const reviewChoices = (choice1, choice2) => {
+    if (choice1 && choice2) {
+      bombCheck(choice1, choice2);
+      matchChecker(choice1, choice2);
+      setTimeout(() => clearChoices(choice1, choice2), 1000);
+
+      setTurns(turns + 1);
+      // console.log(choice1, choice2, turns);
+    }
+  };
+  // bomb function
+  const bombCheck = (choice1, choice2) => {
+    if (choice1 && choice2) {
+      if (choice1.bomb && choice2.bomb) gameOver();
+    }
+  };
+
+  // Game Over function
+  const gameOver = () => {
+    setTimeout(() => {
+      alert("Game Over");
+    }, 1000);
+  };
+  //match checker
+  const matchChecker = (choice1, choice2) => {
     if (choice1 && choice2) {
       setCardEnabler(false);
       if (choice1.src === choice2.src) {
@@ -94,26 +120,21 @@ function App() {
           });
         });
       }
-      setTimeout(() => {
-        setChoice1(null);
-        setChoice2(null);
-        setCardEnabler(true);
-      }, 2000);
     }
   };
 
-  // bomb function
-  const bombCheck = (choice1, choice2) => {
-    if (choice1 && choice2) {
-      if (choice1.bomb && choice2.bomb) gameOver();
+  console.log(cards);
+
+  // clear choices
+  const clearChoices = (choice1, choice2) => {
+    if (choice1 && choice2 !== null) {
+      setChoice1(null);
+      setChoice2(null);
+      setCardEnabler(true);
     }
   };
 
-  // Game Over funtion
-  const gameOver = () => {
-    alert("Game Over");
-  };
-  // game start flipper
+  // starting game card flipper
   const gameStartCardFlipper = () => {
     setCards((prevCards) => {
       return prevCards.map((card) => {
@@ -122,23 +143,30 @@ function App() {
     });
   };
 
+  // starts first game automatically
+
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
   return (
     <div className="App">
-      <h1>Pokemon Match!</h1>
+      <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
       <div className="card-grid">
         {cards.map((card) => (
           <Card
-            key={Math.random()}
-            card={card}
+            key={card.id}
             handleChoice={handleChoice}
+            card={card}
             flipped={card === choice1 || card === choice2 || card.matched}
             enabled={cardEnabler}
           />
         ))}
       </div>
-      {/* <p>Turns : {turns}</p> */}
+      <p>Turns : {turns}</p>
     </div>
   );
 }
+
 export default App;
