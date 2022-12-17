bd4bb85ef0cdacc8ccac3e5d686d42689cc1caad
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-12-17 11:14:00 +0700

    adding gameover styling elements

--- src/components/UI/GameOverModal.js
+++ src/components/UI/GameOverModal.js
@@ -7,6 +7,11 @@ export default function GameOverModal(props) {
   if (!props.open) return null;
   return (
     <div className="modal-wrapper bounce">
+      <img
+        className="game-over-banner"
+        src="./img/game-over-banner.png"
+        alt=""
+      />
       <ScoreStar score={props.score} />
       <div className="endgame-text">
         {/* <p>Your Score is {props.score}</p> */}
