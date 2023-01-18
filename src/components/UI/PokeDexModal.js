0680143dae0373eb1574b2c351e5fedb735fb3cb
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2023-01-18 17:54:54 +0000

    ease in and out animation working

--- src/components/UI/PokeDexModal.js
+++ src/components/UI/PokeDexModal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./CSS/PokeDexModal.css";
 
@@ -6,14 +6,25 @@ import "./CSS/PokeDexModal.css";
 import PokeDexAllMons from "./PokeDexAllMons";
 
 export default function PokedexModal(props) {
-  const handleClick = () => {
-    props.togglePokedex();
+  const [transitionOut, setTransitionOut] = useState(false);
+
+  const closePokedex = () => {
+    setTransitionOut(!transitionOut);
+    setTimeout(() => {
+      props.togglePokedex();
+    }, 1500);
   };
 
   if (!props.open) return null;
   return (
-    <div className="pokedex-modal-container" onClick={handleClick}>
-      <div className="pokedex-modal-wrapper slide-from-left">
+    <div className="pokedex-modal-container" onClick={closePokedex}>
+      <div
+        className={
+          transitionOut
+            ? "pokedex-modal-wrapper slide-out-from-center"
+            : "pokedex-modal-wrapper slide-from-left"
+        }
+      >
         <div className="pokedex-img">
           <div className="pokedex-screen">
             <PokeDexAllMons pullAllMons={props.pullAllMons} />{" "}
