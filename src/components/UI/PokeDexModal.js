73406856819a164ba002671bd9ba5ba57f1d0c95
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2023-01-18 15:45:23 +0000

    CSS added to resize pokedex img

--- src/components/UI/PokeDexModal.js
+++ src/components/UI/PokeDexModal.js
@@ -10,26 +10,12 @@ export default function PokedexModal(props) {
   return (
     <div className="pokedex-modal-container">
       <div className="pokedex-modal-wrapper slide-from-left">
-        <div className="pokedex-body-top">
-          <div className="big-button"></div>
-          <div
-            className="small-button"
-            style={{ backgroundColor: "rgb(181,48,63)" }}
-          ></div>
-          <div
-            className="small-button"
-            style={{ backgroundColor: "rgb(244,246,71)" }}
-          ></div>
-          <div
-            className="small-button"
-            style={{ backgroundColor: "rgb(93,191,117)" }}
-          ></div>
-        </div>
-        <div className="pokedex-body-right"></div>
-        {/* <div className="pokedex-screen">
+        <div className="pokedex-img">
+          {/* <div className="pokedex-screen">
    
         </div>
         <PokeDexAllMons pullAllMons={props.pullAllMons} /> */}
+        </div>
       </div>
     </div>
   );
